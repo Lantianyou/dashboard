e0360991a99c589b9718ae7a2c0c532486839673
兰天游 <dtlantianyou@gmail.com>
2022-12-08 23:50:58 +0800

    feat: boilerplate

---
+++ src/store/index.ts
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { useDispatch } from 'react-redux'
+import { pokemonApi } from './api'
+
+export const store = configureStore({
+  reducer: {
+        [pokemonApi.reducerPath]: pokemonApi.reducer,
+  },
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware),
+})
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch: () => AppDispatch = useDispatch // Export a hook that can be reused to resolve types
